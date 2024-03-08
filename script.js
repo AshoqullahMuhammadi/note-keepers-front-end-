9bd342b94342ac4c5f035de00cac40f12ae1bedb
Ashoqullah <as.coder465@gmail.com>
2024-03-08 12:12:33 +0430

    Completing of CRUD operation and done the ui part also

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 // const overlay = document.querySelector('.overlay');
 
 const addTodoForm = document.querySelector('.add-todo-form');
+const updateNoteForm = document.querySelector('.update-todo-form');
 const todoTitle = document.querySelector('.todo-title');
+const updateNoteTitle = document.querySelector('.update-note-title');
 const todoDescription = document.querySelector('.todo-description');
+const updateNoteDescription = document.querySelector('.update-note-description');
 const addTodo = document.querySelector('.add-todo-form-button');
+const updateNoteButton = document.querySelector('.update-todo-form-button');
 const todoFormCloseButton = document.querySelector('.add-todo-close-button');
+const updateFormCloseButton = document.querySelector('.update-note-close-button');
+const viewContainerCloseButton = document.querySelector('.view-note-close-button');
+
+const overlay = document.querySelector('.overlay');
 
 const showAllNote = document.querySelector('.show-todo-container');
 const showTodo = document.getElementById('show-todo-frame');
@@ -12,12 +20,51 @@ const showTodo = document.getElementById('show-todo-frame');
 const addTodoButton = document.querySelector('.add-note-button');
 const customToast = document.querySelector('.custom-toast');
 const customToastMessage = document.querySelector('.custom-toast p');
+
+
+const confirmationDialog = document.querySelector('.delete-confirmation-dialog');
+const confYesButton = document.querySelector('.yes');
+const confNoButton = document.querySelector('.no');
+
+
+const viewNoteContainer = document.querySelector('.view-note-container');
+const viewNoteContainerTitle = document.querySelector('.view-note-container h2');
+const viewNoteContainerDescription = document.querySelector('.view-note-container p');
 // calling some function
 // functions 
 const closeAddTodoForm = function () {
     addTodoForm.classList.add('hide');
+    overlay.classList.add('hide');
+
     // overlay.classList.remove('hide');
     // addTodoButton.style.display = 'flex';
+}
+
+const closeUpdateForm = function () {
+    updateNoteForm.classList.add('hide');
+    overlay.classList.add('hide');
+
+    // overlay.classList.remove('hide');
+    // addTodoButton.style.display = 'flex';
+}
+
+const closeViewContainer = function () {
+    viewNoteContainer.classList.add('hide');
+    overlay.classList.add('hide');
+
+    // overlay.classList.remove('hide');
+    // addTodoButton.style.display = 'flex';
+}
+const showToast = (message, color) => {
+    overlay.classList.remove('hide');
+    customToastMessage.innerText = message;
+    customToast.style.backgroundColor = color;
+    customToast.style.top = "10px";
+    setTimeout(() => {
+        customToast.style.top = "-120px";
+        overlay.classList.add('hide');
+
+    }, 2000);
 }
 // ? create operations
 const addTodoInDB = async function () {
@@ -36,6 +83,33 @@ const addTodoInDB = async function () {
     const result = await response.json();
 
 }
+// update or edit note
+const editNote = async (id) => {
+
+    const url = `http://localhost:3000/api/v1/note/${id}`;
+    const body = JSON.stringify({
+        note_title: updateNoteTitle.value,
+        note_description: updateNoteDescription.value
+    });
+    try {
+        const response = await fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: body,
+        });
+        const updatedResult = await response.json();
+
+        showToast('update', '#80ffcc')
+
+    } catch (e) {
+        showToast(e, "#FA4B42");
+    }
+
+}
+
+
 
 // get all notes
 const getAllNotes = async function () {
@@ -48,93 +122,201 @@ const getAllNotes = async function () {
         throw new Error("Failed to fetch notes: " + data.message);
     }
 };
+
 const showAllNoteIn = async function () {
     const notes = await getAllNotes();
-    console.log(notes);
 
-    const iframe = document.getElementById('show-todo-frame');
-    const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+    document.querySelector(".container").innerHTML = "";
 
+    const notesContainer = document.createElement('div');
+    // notesContainer.style.height = "100vh";
+    notesContainer.classList.add('scrollable'); // Add the scrollable class to enable scrolling
+    if (notes.length == 0) {
+
+        const textContainer = document.createElement('div');
+        textContainer.innerHTML = "No Note available";
+        textContainer.style = `height:100vh; display: flex; flex-direction: column;justify-content: center; align-items: center;font-size: 30px; color: #ff3300; font-family: sans-serif; font-weight: 500;`;
+        notesContainer.appendChild(textContainer);
+    }
     notes.forEach(note => {
-        // Create card container
-        const card = iframeDoc.createElement('div');
-        card.classList.add('note-card');
-        const editIcon = "./images/edit.png";
-        const infoIcon = "./images/info.png";
-        const deleteIcon = "./images/remove.png";
-        // Create title element
-        const title = iframeDoc.createElement('h2');
-        title.textContent = note.note_title;
-        card.appendChild(title);
+        // create a new Container
+        const noteMainContainer = document.createElement('div');
+        noteMainContainer.classList.add("show-todo-container");
 
-        // Create description element
-        const description = iframeDoc.createElement('p');
-        description.textContent = note.note_description;
-        card.appendChild(description);
 
-        // Create horizontal line
-        const hr = iframeDoc.createElement('hr');
-        card.appendChild(hr);
+        // show title h2 
+        const title = document.createElement('h2');
+        title.innerText = note.note_title;
+        noteMainContainer.appendChild(title);
 
-        // Create buttons container
-        const buttonsContainer = iframeDoc.createElement('div');
+        // description p
+        const description1 = document.createElement('p');
+        description1.innerText = note.note_description;
+        noteMainContainer.appendChild(description1);
+
+        // horizontal line hr
+        const hr = document.createElement('hr');
+        noteMainContainer.appendChild(hr);
+
+
+
+        // buttons container
+        const buttonsContainer = document.createElement('div');
         buttonsContainer.classList.add('buttons-container');
 
-        // Create buttons
-        const editButton = iframeDoc.createElement('div');
-        editButton.innerHTML = `<img src="${editIcon}"/>`;
-        editButton.classList.add('btn');
-        buttonsContainer.appendChild(editButton);
 
-        const deleteButton = iframeDoc.createElement('div');
-        deleteButton.innerHTML = `<img src="${deleteIcon}"/>`;
-        deleteButton.classList.add('btn');
-        buttonsContainer.appendChild(deleteButton);
+        // ? info button
 
-        const detailButton = iframeDoc.createElement('div');
-        detailButton.innerHTML = `<img src="${infoIcon}"/>`;
-        detailButton.classList.add('btn');
-        buttonsContainer.appendChild(detailButton);
+        const infoImage = document.createElement('img');
+        infoImage.src = './images/info.png';
+        infoImage.classList.add('btn');
+        buttonsContainer.appendChild(infoImage);
 
-        card.appendChild(buttonsContainer);
 
-        // Append the card to the iframe document body
-        iframeDoc.body.appendChild(card);
+        infoImage.addEventListener('click', function () {
+            overlay.classList.remove('hide');
+
+            viewNoteContainerTitle.textContent = note.note_title;
+            viewNoteContainerDescription.textContent = note.note_description;
+            viewNoteContainer.classList.remove('hide');
+            viewContainerCloseButton.addEventListener('click', function () {
+                closeViewContainer();
+            });
+            viewNoteContainerTitle.addEventListener('click', function () {
+                viewNoteContainer.classList.add('hide');
+                overlay.classList.remove('hide');
+                showUpdateForm(note._id, note.note_title, note.note_description);
+
+            });
+            viewNoteContainerDescription.addEventListener('click', function () {
+                viewNoteContainer.classList.add('hide');
+                overlay.classList.remove('hide');
+
+                showUpdateForm(note._id, note.note_title, note.note_description);
+
+            });
+            // view note
+
+        });
+
+
+        // ? edit button
+
+        const editImage = document.createElement('img');
+        editImage.src = './images/edit.png';
+        editImage.classList.add('btn');
+        buttonsContainer.appendChild(editImage);
+        // edit note
+        editImage.addEventListener('click', function () {
+            // edit note
+            overlay.classList.remove('hide');
+
+            showUpdateForm(note._id, note.note_title, note.note_description);
+
+        });
+
+
+        // ? delete button
+
+
+        const deleteImage = document.createElement('img');
+        deleteImage.src = './images/remove.png';
+        deleteImage.classList.add('btn');
+        buttonsContainer.appendChild(deleteImage);
+
+        deleteImage.addEventListener('click', function () {
+            confirmationDialog.classList.remove('hide');
+            overlay.classList.remove('hide');
+
+            confYesButton.addEventListener('click', async function () {
+                try {
+
+                    const url = `http://localhost:3000/api/v1/note/${note._id}`;
+
+                    const response = await fetch(url, {
+                        method: 'DELETE'
+                    });
+                    const deleted = await response.json();
+                    confirmationDialog.classList.add('hide');
+                    overlay.classList.add('hide');
+
+                    showToast("Note deleted successfully", "#80ffcc")
+                    showAllNoteIn();
+                } catch (e) {
+                    showToast("Note deleted fail", "#ff6666")
+
+                }
+
+            });
+            confNoButton.addEventListener('click', function () {
+                confirmationDialog.classList.add('hide');
+                overlay.classList.add('hide');
+
+
+            });
+            // delete note
+
+        });
+
+        // buttons creation omitted for brevity
+
+        noteMainContainer.appendChild(buttonsContainer);
+
+        notesContainer.appendChild(noteMainContainer);
     });
+
+    // Append the notes container to an existing container in your HTML, or create a new one if needed
+    const existingContainer = document.querySelector('.container'); // Change '.container' to the selector of your existing container
+    existingContainer.appendChild(notesContainer);
 };
 
-
 showAllNoteIn();
+
+
+const showUpdateForm = function (id, noteTitle, noteDescription) {
+    updateNoteForm.classList.remove("hide");
+
+    updateNoteTitle.value = noteTitle;
+    updateNoteDescription.value = noteDescription;
+    updateNoteButton.addEventListener("click", async function () {
+
+
+        await editNote(id);
+        updateNoteForm.classList.add("hide");
+        updateNoteTitle.value = "";
+        updateNoteDescription.value = "";
+        overlay.classList.add('hide');
+
+        showAllNoteIn();
+    });
+
+}
+
 
 const clearForm = function () {
     todoTitle.value = "";
     todoDescription.value = "";
+
 }
 
-const showToast = (message, color) => {
 
-    customToastMessage.innerText = message;
-    customToast.style.backgroundColor = color;
-    customToast.style.top = "10px";
-    setTimeout(() => {
-        customToast.style.top = "-120px";
-    }, 2000);
-}
 
 addTodoButton.addEventListener('click', function () {
-    // overlay.classList.add('overlay');
+    overlay.classList.remove('hide');
+
     addTodoForm.classList.remove('hide');
 });
 
 
 todoFormCloseButton.addEventListener('click', closeAddTodoForm);
+updateFormCloseButton.addEventListener('click', closeUpdateForm);
 
 addTodo.addEventListener('click', function () {
     addTodo.innerText = "Loading...";
     addTodoInDB();
     addTodo.innerText = "ADD";
-    showToast("Note successfully Added", "#53ff1a");
-
+    showToast("Note successfully Added", "#80ffcc");
+    overlay.classList.add('hide');
     clearForm();
     closeAddTodoForm();
 });
